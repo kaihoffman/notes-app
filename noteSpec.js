c9e0d0fe3cae227aa084dfd509c51f9107e09bed
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-02-18 16:47:23 +0000

    test for adding a note object passes

--- noteSpec.js
+++ noteSpec.js
@@ -7,3 +7,9 @@ function testCreateNoteList() {
     console.log("testCreateNoteList: " + assert.isTrue(noteList != null))
 };
 testCreateNoteList();
+
+function testAddNoteToNoteList() {
+    noteList.addNote(note);
+    console.log("testAddNoteToNoteList: " + assert.isTrue(noteList.showNotes().includes(note)))
+};
+testAddNoteToNoteList();
